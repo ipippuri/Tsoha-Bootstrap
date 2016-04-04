e68db01b46715ea1cf3498176c04b9a8a9a29b6c
ipippuri <iiris.pippuri@gmail.com>
2016-04-04 18:17:07 +0300

    Tietokantataulut lisätty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,25 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO Tutkija (kayttajatunnus, salasana) VALUES ('Tutkija1', 'Tutkija123');
+
+INSERT INTO Kohde (nimi, paikkakunta) VALUES ('Nuuksion Pitkäjärvi', 'Espoo');
+INSERT INTO Kohde (nimi, paikkakunta) VALUES ('Aulangonjärvi', 'Hämeenlinna');
+
+
+INSERT INTO Naytteenottopaikka (kohdeid, leveysaste, pituusaste, maamerkkitieto) VALUES
+            (1, 60.25, 24.6, 'Koivu');
+INSERT INTO Naytteenottopaikka (kohdeid, leveysaste, pituusaste, maamerkkitieto) VALUES
+            (2, 61.027257, 24.472031, 'Kuusi');
+
+
+INSERT INTO Tutkimus (kohdeid, tutkijaid,paivamaara, aistivarainen_tieto, mittaustieto) VALUES
+            (1, 1, '2010-10-10', 'Sään kuvaus', 'Lämpötila 12 astetta');
+INSERT INTO Tutkimus (kohdeid, tutkijaid, paivamaara, aistivarainen_tieto, mittaustieto) VALUES
+            (2, 1, '2011-04-01', 'Veden väri', 'Lämpötila 12 astetta. Veden lämpötila: 2 astetta');
+
+
+INSERT INTO Nayte (n_id, tutkimusid, tutkijaid, nimi, kuvaus, analyysi) VALUES (1, 1, 1,'Happipitoisuus','Veden happipitoisuus','Analyysin tulos tulee tähän.');
+INSERT INTO Nayte (n_id, tutkimusid, tutkijaid, nimi, kuvaus) VALUES (2, 2, 1, 'pH','Veden pH-arvo');
+
+
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,42 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Tutkija(
+    tutkijaid SERIAL PRIMARY KEY,
+    kayttajatunnus varchar(30) UNIQUE NOT NULL,
+    salasana varchar(20) NOT NULL
+);
+
+CREATE TABLE Kohde(
+    kohdeid SERIAL PRIMARY KEY,
+    nimi varchar(50) UNIQUE NOT NULL,
+    paikkakunta varchar(50) NOT NULL
+);
+
+CREATE TABLE Naytteenottopaikka(
+    n_id SERIAL PRIMARY KEY,
+    kohdeid integer REFERENCES Kohde(kohdeid) NOT NULL,
+    leveysaste decimal NOT NULL,
+    pituusaste decimal NOT NULL,
+    maamerkkitieto varchar(150),
+    CHECK (-90 < leveysaste AND leveysaste < 90),
+    CHECK (-180 < pituusaste AND pituusaste < 180)
+);
+
+CREATE TABLE Tutkimus(
+    tutkimusid SERIAL PRIMARY KEY,
+    kohdeid integer REFERENCES Kohde(kohdeid) NOT NULL,
+    tutkijaid integer REFERENCES Tutkija(tutkijaid) NOT NULL,
+    paivamaara date NOT NULL,
+    aistivarainen_tieto text NOT NULL,
+    mittaustieto text NOT NULL
+);
+
+CREATE TABLE Nayte(
+    nayteid SERIAL PRIMARY KEY,
+    n_id integer REFERENCES Naytteenottopaikka(n_id) NOT NULL,
+    tutkimusid integer REFERENCES Tutkimus(tutkimusid) NOT NULL,
+    tutkijaid integer REFERENCES Tutkija(tutkijaid) NOT NULL,
+    nimi varchar(50) NOT NULL,
+    kuvaus text NOT NULL,
+    analyysi text
+);
